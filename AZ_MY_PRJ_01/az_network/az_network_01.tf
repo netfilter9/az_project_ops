a5f2d157d7e9fc040bc0de4d229655fd5e851eca
unknown <netfilter9@gmail.com>
2026-07-03 18:41:03 +0800

    added practice tf - 01

--- AZ_MY_PRJ_01/az_network/az_network_01.tf
+++ AZ_MY_PRJ_01/az_network/az_network_01.tf
@@ -1,13 +1,13 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-terraform-demo"
+  name                = "vnet-stud-01"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-frontend"
-  resource_group_name  = azurerm_resource_group.rg.name
+  name                 = "snet-stud-01"
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
