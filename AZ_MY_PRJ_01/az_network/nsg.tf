d1a34e034d4cc971dcd8c14895b9baec0565b4fd
Cabillar <kenneth.p.cabillar@accenture.com>
2026-07-08 15:47:21 +0800

    added nsg 01

---
+++ AZ_MY_PRJ_01/az_network/nsg.tf
@@ -0,0 +1,24 @@
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-study-vm"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+
+    source_port_range          = "*"
+    destination_port_range     = "22"
+
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
