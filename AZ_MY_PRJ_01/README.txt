405287494b29d725ef815c05a0f752e514fc9a2a
unknown <netfilter9@gmail.com>
2026-07-08 16:33:29 +0800

    compute update 4

--- AZ_MY_PRJ_01/README.txt
+++ AZ_MY_PRJ_01/README.txt
@@ -4,3 +4,6 @@ how to move with this codes follow steps below.
 to make terminal more readable
 type below:
 function prompt { "current> " }
+
+to generate ssh-keys in windows 
+ssh-keygen -t ed25519
---
+++ AZ_MY_PRJ_01/az_network/outputs.tf
@@ -0,0 +1,9 @@
+output "nsg_id" {
+  description = "test "
+  value = azurerm_network_security_group.nsg.id 
+}  
+
+output "nsg_name" {
+  description = "test "
+  value = azurerm_network_security_group.nsg.name
+}  
