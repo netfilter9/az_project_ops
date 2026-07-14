705ad381130440c45d86014e719f206de4b2b855
unknown <netfilter9@gmail.com>
2026-07-14 21:20:22 +0800

    working vm deployment 71426

--- AZ_MY_PRJ_01/az_compute_00/az_compute_00.tf
+++ AZ_MY_PRJ_01/az_compute_00/az_compute_00.tf
@@ -1,17 +1,16 @@
 
 
-resource "azurerm_virtual_network" "vnet" {
+# Look up the existing VNet
+data "azurerm_virtual_network" "vnet" {
   name                = "vnet-stud-01"
-  address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "subnet" {
+# Look up the existing Subnet inside that VNet
+data "azurerm_subnet" "subnet" {
   name                 = "snet-stud-01"
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
   resource_group_name  = data.azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "pip" {
@@ -29,17 +28,17 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
+    subnet_id                     = data.azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "vm-study-b1ms-00"
+  name                = "vm-study-b1ms-00" 
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
-  size                = "Standard_B1ms"
+  size                = "Standard_B2ats_v2" //Standard_B2ats_v2 old Standard_B1ms
 
   admin_username = var.admin_username
 
