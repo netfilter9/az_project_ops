cd0b955840da26032a1d576f191d8a8f3bf44219
Cabillar <kenneth.p.cabillar@accenture.com>
2026-07-14 21:35:54 +0800

    move all data in datatf

--- AZ_MY_PRJ_01/az_compute_00/az_compute_00.tf
+++ AZ_MY_PRJ_01/az_compute_00/az_compute_00.tf
@@ -1,18 +1,3 @@
-
-
-# Look up the existing VNet
-data "azurerm_virtual_network" "vnet" {
-  name                = "vnet-stud-01"
-  resource_group_name = data.azurerm_resource_group.rg.name
-}
-
-# Look up the existing Subnet inside that VNet
-data "azurerm_subnet" "subnet" {
-  name                 = "snet-stud-01"
-  virtual_network_name = data.azurerm_virtual_network.vnet.name
-  resource_group_name  = data.azurerm_resource_group.rg.name
-}
-
 resource "azurerm_public_ip" "pip" {
   name                = "pip-study-vm-00"
   location            = data.azurerm_resource_group.rg.location
--- AZ_MY_PRJ_01/az_compute_00/data.tf
+++ AZ_MY_PRJ_01/az_compute_00/data.tf
@@ -1,3 +1,15 @@
 data "azurerm_resource_group" "rg" {
   name = "rg-az-stud-01"
 }
+# Look up the existing VNet
+data "azurerm_virtual_network" "vnet" {
+  name                = "vnet-stud-01"
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+# Look up the existing Subnet inside that VNet
+data "azurerm_subnet" "subnet" {
+  name                 = "snet-stud-01"
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
+}
